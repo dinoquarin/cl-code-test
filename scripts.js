6423a8dcc93d3b8b717425143e26f9c880832878
Dino Quarin <contact@designbyddc.com>
2020-10-03 17:52:25 -0400

    Get all variables from JSON file

--- scripts.js
+++ scripts.js
@@ -13,11 +13,36 @@ fetch(myRequest)
     .then(function(data){
 
 
-
+        // display user profile information
         const userPhoto = data.profile_picture;
+        const userName = data.name;
+        const userEmail = data.email;   
+        const userPhone = data.phone;   
+        const userBio = data.bio; 
+
+
         document.getElementById('user-profile__container').innerHTML += ` 
         <img src="${userPhoto}" />
+        <h1>${userName }</h1>
+        ${userBio}</br>
+        ${userPhone}</br>
+        ${userEmail}
         `;
+
+
+        //get photos
+        const photosList = data.album;
+
+        Object.keys(photosList).forEach(function (item) {
+            document.getElementById('photos-card__container').innerHTML += `
+            <img src="${photosList[item].img}" />
+            ${photosList[item].title}<br/>
+            ${photosList[item].description}<br/>
+            ${photosList[item].featured}<br/>
+            ${photosList[item].date}
+            `;
+        });    
+
 
     // end fetch request    
     });
