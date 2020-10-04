6b500a42c9cc5b4f75d636c009691f8141836fe5
Dino Quarin <contact@designbyddc.com>
2020-10-03 22:34:20 -0400

    script and css update
    finshed layout with svg heart added

--- scripts.js
+++ scripts.js
@@ -23,15 +23,17 @@ fetch(myRequest)
 
         document.getElementById('user-profile__container').innerHTML += ` 
         <div class="user-profile--img"><img src="${userPhoto}" /></div>
-            <div class="user-profile--bio-container">
-                <h1 class="user-profile--name">${userName }</h1>
-                <div class="user-profile__bio">
-                    <div class="bio--title">Bio</div>
-                    ${userBio}
-                </div>
+        <div class="user-profile--bio-container">
+            <h1 class="user-profile--name">${userName }</h1>
+            <div class="user-profile__bio">
+                <div class="bio--title">Bio</div>
+                ${userBio}
             </div>
-        ${userPhone}</br>
-        ${userEmail}
+        </div>
+        <div class="user-profile--contact">
+            <div class="user-profile--phone"><div>Phone</div>${userPhone}</div>
+            <div class="user-profile--email"><div>Email</div>${userEmail}</div>   
+        </div>
         `;
 
 
@@ -47,8 +49,8 @@ fetch(myRequest)
                 </div>
                 <div class="photos-card--description">
                 <p>${photosList[item].description}</p>
-                <div class="photos-card--featured">${photosList[item].featured}</div>
-                <div class="photos-card--date">${photosList[item].date}</div>
+                <div class="photos-card--featured"><i class="featured-heart ${photosList[item].featured}"></i></div>
+                    <div class="photos-card--date">${photosList[item].date}</div>
             </div>
             </div>
            
@@ -59,6 +61,6 @@ fetch(myRequest)
             // ${photosList[item].description}<br/>
             // ${photosList[item].featured}<br/>
             // ${photosList[item].date}
-            
+
     // end fetch request    
     });
